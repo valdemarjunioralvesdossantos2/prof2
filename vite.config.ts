8415f8ffcf25d234b0052170e79cb1fc5036ecee
valdemarjunioralvesdossantos2 <vjalvesui@hotmail.com>
2026-02-10 03:23:41 -0300

    mudando a pasta do build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+  // Defina aqui o nome da nova pasta
+    outDir: 'build', 
+  }
 })
