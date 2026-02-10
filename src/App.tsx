bac1f65ba282bf34dc1ea29d2ed8eb630877aeee
valdemarjunioralvesdossantos2 <vjalvesui@hotmail.com>
2026-02-10 17:51:35 -0300

    Começando a gerar o layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import pagina1 from './assets/convite1.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  //  const myStyle = {
+  //   backgroundImage: `url(${pagina1})`,
+  //   backgroundSize: '100%', // Add other CSS properties as needed
+  //   backgroundRepeat: 'no-repeat'
+  // }
 
   return (
     <>
-      <div>
+      <div className="fundo" >
+        <img src={pagina1} className="pag1" alt="React logo" />
+      </div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +35,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p>  */}
     </>
   )
 }
