05fd70ac80ba10ff6f718d0ff0c415c85d7338ca
valdemarjunioralvesdossantos2 <vjalvesui@hotmail.com>
2026-02-18 00:29:33 -0300

    Versão para os padrinhos e madrinhas solteiras Melhorias no nome do site

--- src/components/PadrinhosPg2/PadrinhosPg2.tsx
+++ src/components/PadrinhosPg2/PadrinhosPg2.tsx
@@ -4,21 +4,31 @@ import './PadrinhosPg2.css';
 
 interface PadrinhosPg2Props {madrinha: string | null, padrinho: string | null}
 
-const PadrinhosPg2: FC<PadrinhosPg2Props> = (props) => (
- <div>    
-        {/* <img src={pagina1} className="pag1" alt="React logo" /> */}
-        <div className='fonteAnastasia'>
-         {props.madrinha} & {props.padrinho}
-        </div>
-        <img src={pagina2} className="pag1" alt="React logo" />
-        <div className='fonteAnastasia2'>
-         Aceitam ser nossos<br /> 
-         <div className='padrinhos'>
-            padrinhos
-            <div className='interroga'>?</div>
-         </div>
-        </div>
- </div>
-);
+const PadrinhosPg2: FC<PadrinhosPg2Props> = function (props) { 
+ const soMadPreenchida = props.madrinha !== null &&  props.padrinho === null;
+ const soPadPreenchida = props.padrinho !== null && props.madrinha === null;
+ const ambosPreenchidos = props.madrinha !== null && props.padrinho !== null;
 
+ return (   
+   <div>    
+         {/* <img src={pagina1} className="pag1" alt="React logo" /> */}
+         <div className='fonteAnastasia'>
+            {props.madrinha} {ambosPreenchidos && '&'} {props.padrinho}
+         </div>
+         <img src={pagina2} className="pag1" alt="React logo" />
+         <div className='fonteAnastasia2'>            
+            {soMadPreenchida && 'Aceita ser nossa'}
+            {soPadPreenchida && 'Aceita ser nosso'}
+            {ambosPreenchidos && 'Aceitam ser nossos'}
+           <br /> 
+            <div className='padrinhos'>
+               {soMadPreenchida && 'madrinha'}
+               {soPadPreenchida && 'padrinho'}
+               {ambosPreenchidos && 'padrinhos'}
+               <div className='interroga'>?</div>
+            </div>
+         </div>
+   </div>
+ )
+}
 export default PadrinhosPg2;
