ad26c053eafbf5ebf02a0d2ef445789c0d582dca
valdemarjunioralvesdossantos2 <vjalvesui@hotmail.com>
2026-03-31 12:42:09 -0300

    Ajustando a pagina 2 do preconvite

--- src/components/Imersos/ImersosPg2/ImersosPg2.tsx
+++ src/components/Imersos/ImersosPg2/ImersosPg2.tsx
@@ -7,14 +7,15 @@ const ImersosPg2: FC = function () {
 
  return (   
    <div className='divpag2Imersos'>    
-         <div className='topoImersos'>
-            Com muito carinho, queremos compartilhar um momento especial das nossas vidas com você!
+         <div className='topoImersosPg2'>
+            Com muito carinho <br/>
+            Queremos compartilhar um momento especial das nossas vidas com você!
             <img src={noivosImersos} alt="React logo" />
             Nosso casamento acontecerá no dia 07 de novembro de 2026.
            
          </div>
          <div className='rodapeImersos'>
-            <div className='versiculoImersos'>
+            <div className='versiculoImersosPg2'>
                Colossenses 3:14<br />
                E, sobre tudo isto, revesti-vos de amor, que é o vínculo da perfeição.
             </div>
